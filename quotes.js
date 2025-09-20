ed2c2ac3ead71ba2cf294bfbc0ac3ca960952d30
zakkalashnyk <akmzahar@gmail.com>
2025-09-20 12:39:07 +0300

    Added functionality for creating an favorite cards. Updated UI/UX

--- quotes.js
+++ quotes.js
@@ -3,108 +3,175 @@ const quotes = [
     quote:
       "The only limit to our realization of tomorrow is our doubts of today.",
     author: "Franklin D. Roosevelt",
+    isFavorite: false,
   },
   {
     quote: "In the middle of every difficulty lies opportunity.",
     author: "Albert Einstein",
+    isFavorite: false,
   },
-  { quote: "Happiness depends upon ourselves.", author: "Aristotle" },
-  { quote: "Turn your wounds into wisdom.", author: "Oprah Winfrey" },
+  {
+    quote: "Happiness depends upon ourselves.",
+    author: "Aristotle",
+    isFavorite: false,
+  },
+  {
+    quote: "Turn your wounds into wisdom.",
+
+    author: "Oprah Winfrey",
+    isFavorite: false,
+  },
   {
     quote: "Do what you can, with what you have, where you are.",
     author: "Theodore Roosevelt",
+    isFavorite: false,
   },
   {
     quote:
       "Success is not final, failure is not fatal: It is the courage to continue that counts.",
     author: "Winston Churchill",
+    isFavorite: false,
   },
   {
     quote: "It always seems impossible until it’s done.",
     author: "Nelson Mandela",
+    isFavorite: false,
   },
   {
     quote: "Don’t count the days, make the days count.",
     author: "Muhammad Ali",
+    isFavorite: false,
   },
   {
     quote: "Act as if what you do makes a difference. It does.",
     author: "William James",
+    isFavorite: false,
   },
   {
     quote: "The best way to predict your future is to create it.",
     author: "Peter Drucker",
+    isFavorite: false,
   },
   {
     quote: "You miss 100% of the shots you don’t take.",
     author: "Wayne Gretzky",
+    isFavorite: false,
   },
-  { quote: "Dream big and dare to fail.", author: "Norman Vaughan" },
+  {
+    quote: "Dream big and dare to fail.",
+    isFavorite: false,
+    author: "Norman Vaughan",
+  },
   {
     quote: "Change your thoughts and you change your world.",
     author: "Norman Vincent Peale",
+    isFavorite: false,
   },
   {
     quote: "The journey of a thousand miles begins with a single step.",
     author: "Lao Tzu",
+    isFavorite: false,
   },
   {
     quote: "Don’t watch the clock; do what it does. Keep going.",
     author: "Sam Levenson",
+    isFavorite: false,
   },
   {
     quote:
       "What you get by achieving your goals is not as important as what you become by achieving your goals.",
     author: "Zig Ziglar",
+    isFavorite: false,
   },
   {
     quote: "Everything you’ve ever wanted is on the other side of fear.",
     author: "George Addair",
+    isFavorite: false,
   },
   {
     quote: "Opportunities don't happen. You create them.",
     author: "Chris Grosser",
+    isFavorite: false,
   },
   {
     quote: "Believe you can and you're halfway there.",
     author: "Theodore Roosevelt",
+    isFavorite: false,
   },
   {
     quote: "Start where you are. Use what you have. Do what you can.",
     author: "Arthur Ashe",
+    isFavorite: false,
   },
   { quote: "Stay hungry, stay foolish.", author: "Steve Jobs" },
   {
     quote: "Simplicity is the ultimate sophistication.",
     author: "Leonardo da Vinci",
+    isFavorite: false,
   },
-  { quote: "Less is more.", author: "Ludwig Mies van der Rohe" },
-  { quote: "Fortune favors the bold.", author: "Virgil" },
+  {
+    quote: "Less is more.",
+    isFavorite: false,
+    author: "Ludwig Mies van der Rohe",
+  },
+  { quote: "Fortune favors the bold.", isFavorite: false, author: "Virgil" },
   {
     quote: "Be yourself; everyone else is already taken.",
     author: "Oscar Wilde",
+    isFavorite: false,
   },
-  { quote: "Knowledge is power.", author: "Francis Bacon" },
-  { quote: "Well done is better than well said.", author: "Benjamin Franklin" },
-  { quote: "Time is money.", author: "Benjamin Franklin" },
-  { quote: "Power tends to corrupt.", author: "Lord Acton" },
+  { quote: "Knowledge is power.", isFavorite: false, author: "Francis Bacon" },
+  {
+    quote: "Well done is better than well said.",
+    isFavorite: false,
+    author: "Benjamin Franklin",
+  },
+  { quote: "Time is money.", isFavorite: false, author: "Benjamin Franklin" },
+  { quote: "Power tends to corrupt.", isFavorite: false, author: "Lord Acton" },
   {
     quote: "Life is short. Smile while you still have teeth.",
     author: "Unknown",
+    isFavorite: false,
   },
-  { quote: "Hope is a waking dream.", author: "Aristotle" },
-  { quote: "Silence is golden.", author: "Proverb" },
+  { quote: "Hope is a waking dream.", isFavorite: false, author: "Aristotle" },
+
+  { quote: "Silence is golden.", isFavorite: false, author: "Proverb" },
   {
     quote: "Do it with passion or not at all.",
     author: "Rosa Nouchette Carey",
+    isFavorite: false,
   },
-  { quote: "Dream without fear. Love without limits.", author: "Unknown" },
-  { quote: "Die with memories, not dreams.", author: "Unknown" },
-  { quote: "Work hard. Stay humble.", author: "Unknown" },
-  { quote: "Collect moments, not things.", author: "Unknown" },
-  { quote: "No pressure, no diamonds.", author: "Thomas Carlyle" },
-  { quote: "Courage is grace under pressure.", author: "Ernest Hemingway" },
-  { quote: "Do small things with great love.", author: "Mother Teresa" },
+  {
+    quote: "Dream without fear. Love without limits.",
+    isFavorite: false,
+    author: "Unknown",
+  },
+  {
+    quote: "Die with memories, not dreams.",
+    isFavorite: false,
+    author: "Unknown",
+  },
+  { quote: "Work hard. Stay humble.", isFavorite: false, author: "Unknown" },
+  {
+    quote: "Collect moments, not things.",
+    isFavorite: false,
+    author: "Unknown",
+  },
+  {
+    quote: "No pressure, no diamonds.",
+    isFavorite: false,
+    author: "Thomas Carlyle",
+  },
+  {
+    quote: "Courage is grace under pressure.",
+    isFavorite: false,
+    author: "Ernest Hemingway",
+  },
+  {
+    quote: "Do small things with great love.",
+    isFavorite: false,
+    author: "Mother Teresa",
+  },
 ];
 
 export default quotes;
--- script.js
+++ script.js
@@ -3,23 +3,46 @@ import quotes from "./quotes.js";
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("quote-author");
 const generateBtn = document.getElementById("generate-btn");
+const toggleCollectionBtn = document.getElementById("toggle-favorite-btn");
+const favoritesContainer = document.getElementById("favorites-container");
+
+let currentQuoteIndex;
 
 function generateRandomQuotes() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
-  // option 1
-  // const { quote, author} = randomQuote;
-  // quoteElement.textContent = quote;
-  // quoteAuthorElement.textContent = author;
-  //
-  // option 2
-  // const { quote, author} = quotes[randomIndex];
-  // quoteElement.textContent = quote;
-  // quoteAuthorElement.textContent = author;
-
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   quoteElement.textContent = randomQuote.quote;
   quoteAuthorElement.textContent = randomQuote.author;
+  toggleCollectionBtn.textContent = randomQuote.isFavorite
+    ? "remove from collection"
+    : "Add to collection";
+  toggleCollectionBtn.style.display = "inline-block";
+}
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  // console.log(currentQuote);
+  toggleCollectionBtn.textContent = currentQuote.isFavorite
+    ? "remove from collection"
+    : "Add to collection";
+
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement("div");
+    favoriteCard.classList.add("favorite-card");
+    favoriteCard.innerHTML = `<p>${currentQuote.quote}<p>
+  <p class = "author">${currentQuote.author} </p>`;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    //removes card if unfovorited
+    const favoriteCard = document.querySelectorAll(".favorite-card");
+    favoriteCard.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove();
+      }
+    });
+  }
 }
 
 generateBtn.addEventListener("click", generateRandomQuotes);
+toggleCollectionBtn.addEventListener("click", toggleFavorite);
 // generateRandomQuotes();
