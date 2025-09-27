5a89a8422c1121f80e95c2f5ad9a5f27e52a2cf1
zakkalashnyk <akmzahar@gmail.com>
2025-09-27 17:09:29 +0300

    Removing unfavorite card from DOM by id.

--- src/handlers/favorites.js
+++ src/handlers/favorites.js
@@ -1,14 +1,13 @@
+// import { cloneElement } from "react";
 import { favoriteBtn } from "../../index.js";
 
 function toggleFavorite(quote, btn, container) {
   quote.isFavorite = !quote.isFavorite;
-  const { text, author, isFavorite } = quote;
-  toggleFavoriteBtnIcon(isFavorite, btn);
-
-  if (isFavorite) {
-    showFavoriteCard(text, author, container);
+  toggleFavoriteBtnIcon(quote.isFavorite, btn);
+  if (quote.isFavorite) {
+    showFavoriteCard(quote, container);
   } else {
-    hideFavoriteCard(text);
+    hideFavoriteCard(quote.id);
   }
 }
 
@@ -30,21 +29,21 @@ function hideFavoriteBtn(btn) {
   btn.style.display = "none";
 }
 
-function showFavoriteCard(text, author, container) {
+function showFavoriteCard(quote, container) {
+  const { id, text, author } = quote;
   const favoriteCard = document.createElement("div");
   favoriteCard.classList.add("favorite-card");
+  favoriteCard.dataset.quoteId = id;
   favoriteCard.innerHTML = `<p>${text}<p>
   <p class = "author">${author} </p>`;
   container.appendChild(favoriteCard);
 }
 
-function hideFavoriteCard(text) {
-  const favoriteCard = document.querySelectorAll(".favorite-card");
-  favoriteCard.forEach((card) => {
-    if (card.textContent.includes(text)) {
-      card.remove();
-    }
-  });
+function hideFavoriteCard(id) {
+  const card = document.querySelector(`.favorite-card[data-quote-id="${id}"]`);
+  if (card) {
+    card.remove();
+  }
 }
 
 export { handleFavorite, toggleFavorite, hideFavoriteBtn };
